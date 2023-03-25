db09f307d8fd6d373247c9d7191753dd487cb71b
Szymon Wrzesiński <236706@edu.p.lodz.pl>
2023-03-25 10:39:13 +0100

    maze spawning complete

--- Maze.py
+++ Maze.py
@@ -3,23 +3,35 @@ import random
 
 class Maze:
     size = 12
-    player = 0
-    elevation = 0
-    doors = 0
-    keys = 0
+    player = []
+    elevation = []
+    doors = []
+    keys = []
     isDoors = False
     doorArr = [False, False, False]
 
+    def __init__(self, size, isDoors):
+        print("starting constructor")
+        self.size = size
+        self.isDoors = isDoors
+        self.initPlayer()
+        self.initElevation()
+        if isDoors:
+            self.initDoors()
+            self.initKeys()
+
     def initPlayer(self):
+        print("initing player")
         self.player = [[0 for x in range(self.size)] for y in range(self.size)]
-        self.player[random.randint(0, self.size-1)][random.randint(0, self.size-1)] = 1
+        self.player[random.randint(0, self.size - 1)][random.randint(0, self.size - 1)] = 1
 
         while True:
-            x = random.randint(0, self.size-1)
-            y = random.randint(0, self.size-1)
+            x = random.randint(0, self.size - 1)
+            y = random.randint(0, self.size - 1)
             if self.player[x][y] != 1:
                 self.player[x][y] = 2
                 break
+        print("finished initing player")
 
     def initElevation(self):
         self.elevation = [[random.randint(1, 6) for x in range(self.size)] for y in range(self.size)]
@@ -32,11 +44,35 @@ class Maze:
             for i in range(doorNumber):
                 self.doorArr[i] = True
 
-        for i in range(doorNumber+1):
-            while True:
-                x = random.randint(0, self.size-1)
-                y = random.randint(0, self.size-1)
-                if self.doors[x][y] == 0:
-                    self.doors[x][y] = i
-                    break
+        for i in range(doorNumber + 1):
+            for x in range(random.randint(1, self.size)):
+                while True:
+                    x = random.randint(0, self.size - 1)
+                    y = random.randint(0, self.size - 1)
+                    if self.doors[x][y] == 0 and self.player[x][y] == 0:
+                        self.doors[x][y] = i
+                        break
 
+    def initKeys(self):
+        self.keys = [[0 for x in range(self.size)] for y in range(self.size)]
+
+        for i in range(len(self.doorArr)):
+            if self.doorArr[i]:
+                while True:
+                    x = random.randint(0, self.size - 1)
+                    y = random.randint(0, self.size - 1)
+                    if self.keys[x][y] == 0 and self.player[x][y] == 0 and self.doors[x][y] == 0:
+                        self.keys[x][y] = i+1
+                        break
+
+    def toString(self):
+        print("PLAYER")
+        print('\n'.join(' '.join(str(x) for x in row) for row in self.player))
+        print("\nELEVATION")
+        print('\n'.join(' '.join(str(x) for x in row) for row in self.elevation))
+        if self.isDoors:
+            # print(self.doorArr)
+            print("\nDOORS")
+            print('\n'.join(' '.join(str(x) for x in row) for row in self.doors))
+            print("\nKEYS")
+            print('\n'.join(' '.join(str(x) for x in row) for row in self.keys))
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import Maze
+from Maze import Maze
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    maze = Maze.Maze()
-    maze.initDoors()
-    print('\n'.join(' '.join(str(x) for x in row) for row in maze.doors))
+    maze = Maze(4, True)
+    maze.toString()
+
+
