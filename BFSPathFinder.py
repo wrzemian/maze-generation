18ce601121bf4b322dd561c1252b4c13771e67df
Szymon Wrzesiński <236706@edu.p.lodz.pl>
2023-08-28 22:00:49 +0200

    cleaned up code

--- BFSPathFinder.py
+++ BFSPathFinder.py
@@ -1,46 +1,40 @@
 from collections import deque
 
+
 class BFSPathFinder:
     def __init__(self, maze):
         self.maze = maze
 
     def find_shortest_path(self):
         if not self.maze.startingPoint or not self.maze.endingPoint:
-            return []  # Return an empty list if starting or ending point is not set.
+            return []
 
-        # Define the movement directions (up, down, left, right).
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         i = 0
-        while i < 3:
+        while i < 10:
             queue = deque()
             queue.append((self.maze.startingPoint[0], self.maze.startingPoint[1], []))
 
             while queue:
-                # print(len(queue))
                 row, col, path = queue.popleft()
 
                 if row == self.maze.endingPoint[0] and col == self.maze.endingPoint[1]:
                     return path
 
-                # Explore adjacent cells.
                 for dr, dc in directions:
                     new_row, new_col = row + dr, col + dc
 
                     if self.isValid(new_row, new_col, row, col, path, i):
                         queue.append((new_row, new_col, path + [(row, col)]))
-                        # print(path + [(row, col)])
             i = i + 1
-        return []  # Return an empty list if no valid path is found.
+        return []
 
     def isValid(self, new_row, new_col, row, col, path, i):
         if not (0 <= new_row < self.maze.size and 0 <= new_col < self.maze.size):
             return False
         if not (abs(self.maze.elevation[new_row][new_col] - self.maze.elevation[row][col]) <= 2):
             return False
-        # if not (path.count((new_row, new_col)) <= i):
-        #     continue
-        # print("TRYING FOR i:", i)
         if path.count((new_row, new_col)) >= i:
             return False
         if not (self.maze.doors[new_row][new_col] == 0):
@@ -57,8 +51,6 @@ class BFSPathFinder:
 
     def can_use_doors(self, keyType, visitedSpots):
         keys_to_use = self.maze.keysArr[keyType - 1]
-        # print("AAAAAAA: ", keys_to_use)
-        # print("VISITED: ", visitedSpots)
         if len(keys_to_use) == 0:
             return False
         for key_cell in keys_to_use:
